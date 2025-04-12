fafa2fb74d63156c66b93d7da3778621061b401c
Tuan Anh <pewpewsts@gmail.com>
2025-04-12 10:15:52 +0700

    rebase

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -7,17 +7,7 @@ import AuthLayout from '../layouts/AuthLayout/AuthLayout';
 
 // Pages
 import HomePage from '../pages/HomePage/HomePage';
-// import LoginPage from '../pages/LoginPage/LoginPage';
-// import RegisterPage from '../pages/RegisterPage/RegisterPage';
-// import DashboardPage from '../pages/DashboardPage/DashboardPage';
-// import AccountsPage from '../pages/AccountsPage/AccountsPage';
-// import AccountDetailsPage from '../pages/AccountDetailsPage/AccountDetailsPage';
-// import CustomersPage from '../pages/CustomersPage/CustomersPage';
-// import CustomerDetailsPage from '../pages/CustomerDetailsPage/CustomerDetailsPage';
-// import ProjectsPage from '../pages/ProjectsPage/ProjectsPage';
-// import ProjectDetailsPage from '../pages/ProjectDetailsPage/ProjectDetailsPage';
-// import ContractsPage from '../pages/ContractsPage/ContractsPage';
-// import ContractDetailsPage from '../pages/ContractDetailsPage/ContractDetailsPage';
+
 
 // Context
 import { useAuth } from '../hooks/useAuth';
@@ -32,31 +22,7 @@ const AppRouter = () => {
                 <Route index element={<HomePage />} />
             </Route>
 
-            {/* Auth routes */}
-            {/* <Route path="auth" element={<AuthLayout />}>
-                <Route path="login" element={<LoginPage />} />
-                <Route path="register" element={<RegisterPage />} />
-            </Route> */}
 
-            {/* Protected routes */}
-            {/* <Route
-                path="dashboard"
-                element={
-                    isAuthenticated ? <MainLayout /> : <Navigate to="/auth/login" />
-                }
-            >
-                <Route index element={<DashboardPage />} />
-                <Route path="accounts" element={<AccountsPage />} />
-                <Route path="accounts/:id" element={<AccountDetailsPage />} />
-                <Route path="customers" element={<CustomersPage />} />
-                <Route path="customers/:id" element={<CustomerDetailsPage />} />
-                <Route path="projects" element={<ProjectsPage />} />
-                <Route path="projects/:id" element={<ProjectDetailsPage />} />
-                <Route path="contracts" element={<ContractsPage />} />
-                <Route path="contracts/:id" element={<ContractDetailsPage />} />
-            </Route> */}
-
-            {/* Catch all */}
             <Route path="*" element={<Navigate to="/" />} />
         </Routes>
     );
